b246e3c1d4307bad7602adbbbcb22e992996d729
Amim Knabben <amim.knabben@gmail.com>
2020-03-23 21:02:22 -0400

    Init tracer

---
+++ telemetry/settings.py
@@ -0,0 +1,6 @@
+DEFAULTS = {
+    "JAEGER_HOST": "localhost",
+    "JAEGER_PORT": 6831,
+    "SERVICE_NAME": "wsgi"
+}
+
---
+++ telemetry/tracer.py
@@ -0,0 +1,19 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+
+
+def init_tracer():
+    from opentelemetry.ext.jaeger import JaegerSpanExporter
+
+    exporter = JaegerSpanExporter(
+        service_name="basic-1", agent_host_name="localhost", agent_port=6831,
+    )
+
+    trace.set_tracer_provider(TracerProvider())
+    span_processor = BatchExportSpanProcessor(exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
+
+
+def get_tracer():
+    return trace.get_tracer(__name__)
